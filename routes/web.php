709fdae2217c44897db34a96399708f21d470eaf
Matheus França <matheussss6613@gmail.com>
2023-07-03 07:19:57 -0300

    Rota de teste

--- routes/web.php
+++ routes/web.php
@@ -25,7 +25,14 @@ use Illuminate\Support\Facades\Route;
 // });
     
 Route::get('/', [\App\Http\Controllers\PrincipalController::class, 'principal']);
+
 Route::get('/sobre-nos', [\App\Http\Controllers\SobreNosController::class, 'sobreNos']);
+
 Route::get('/contato', [\App\Http\Controllers\ContatoController::class, 'contato']);
+//nome, categoria, assunto, mensagem
 
 
+Route::get('contato/{nome}/{categoria}/{assunto}/{mensagem}', function(string $nome, string $categoria, string $assunto, string $mensagem) 
+{
+    echo "teste contato: $nome - $categoria - $assunto - $mensagem";
+}); //teste
