9464564faccfa45251174c8bfe01eeddd22156be
Matheus França <matheussss6613@gmail.com>
2023-07-03 08:02:19 -0300

    add link de route

--- resources/views/site/contato.blade.php
+++ resources/views/site/contato.blade.php
@@ -1,1 +1,13 @@
 <h3>Contato (view)</h3>
+
+<ul>
+    <li>
+        <a href="/">Principal</a>
+    </li>
+    <li>
+        <a href="/contato">Contato</a>
+    </li>
+    <li>
+        <a href="/sobre-nos">Sobre Nós</a>
+    </li>
+</ul>
--- resources/views/site/sobre-nos.blade.php
+++ resources/views/site/sobre-nos.blade.php
@@ -1,1 +1,13 @@
 <h3>Sobre nós (view) </h3>
+
+<ul>
+    <li>
+        <a href="/">Principal</a>
+    </li>
+    <li>
+        <a href="/contato">Contato</a>
+    </li>
+    <li>
+        <a href="/sobre-nos">Sobre Nós</a>
+    </li>
+</ul>
