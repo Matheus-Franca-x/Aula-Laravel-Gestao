37ef3a60aadb2cb5a1b508587ad4334e8d50dad7
Matheus França <matheussss6613@gmail.com>
2023-07-05 08:43:41 -0300

    add nome fixo de rota e novas rotas

--- routes/web.php
+++ routes/web.php
@@ -23,16 +23,21 @@ use Illuminate\Support\Facades\Route;
 // Route::get('/contato', function () {
 //     return "Contato";
 // });
+
+// Route::get('contato/{nome}/{categoria_id}', function(string $nome = "Desconhecido", int $categoria_id = 0) 
+// {
+//     echo "teste contato: $nome - $categoria_id";
+// }
+// )->where('categoria_id', '[0-9]+')->where('nome', '[A-Za-z]+'); //teste
     
-Route::get('/', [\App\Http\Controllers\PrincipalController::class, 'principal']);
+Route::get('/', [\App\Http\Controllers\PrincipalController::class, 'principal'])->name('site.index');
+Route::get('/sobre-nos', [\App\Http\Controllers\SobreNosController::class, 'sobreNos'])->name('site.sobrenos');
+Route::get('/contato', [\App\Http\Controllers\ContatoController::class, 'contato'])->name('site.contato');
+Route::get('/login', function() {return "login";})->name('site.login');
 
-Route::get('/sobre-nos', [\App\Http\Controllers\SobreNosController::class, 'sobreNos']);
-
-Route::get('/contato', [\App\Http\Controllers\ContatoController::class, 'contato']);
-//nome, categoria, assunto, mensagem
-
-
-Route::get('contato/{nome}/{categoria}/{assunto}/{mensagem}', function(string $nome, string $categoria, string $assunto, string $mensagem) 
+Route::prefix('/app')->group(function() 
 {
-    echo "teste contato: $nome - $categoria - $assunto - $mensagem";
-}); //teste
+    Route::get('/clientes', function() {return "clientes";})->name('app.clientes');
+    Route::get('/fornecedores', function() {return "fornecedores";})->name('app.fornecedores');
+    Route::get('/produtos', function() {return "produtos";})->name('app.produtos');
+});
