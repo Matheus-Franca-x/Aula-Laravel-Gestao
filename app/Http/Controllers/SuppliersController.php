0f002c00db81de1f7c804b0cbbeafdcf4358b779
Matheus França <matheussss6613@gmail.com>
2023-07-10 15:49:10 -0300

    add cnpj na variável 0

--- app/Http/Controllers/SuppliersController.php
+++ app/Http/Controllers/SuppliersController.php
@@ -10,10 +10,11 @@ class SuppliersController extends Controller
     public function index()
     {
         $suppliers = [
-            0 => ['nome' => 'fornecedor 1', 'status' => 'N'],
+            0 => ['nome' => 'fornecedor 1', 'status' => 'N', 'cnpj' => '00.000.000/000-00'],
             1 => ['nome' => 'fornecedor 2', 'status' => 'S']
         ];
 
         return view('app.suppliers.index', compact('suppliers'));
+        // return view('app.suppliers.index');
     }
 }
