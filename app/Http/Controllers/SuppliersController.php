ade22560d87e998120a92b6e5e21d960f4b16b66
Matheus França <matheussss6613@gmail.com>
2023-07-10 17:14:51 -0300

    cnpj '' para test

--- app/Http/Controllers/SuppliersController.php
+++ app/Http/Controllers/SuppliersController.php
@@ -10,7 +10,7 @@ class SuppliersController extends Controller
     public function index()
     {
         $suppliers = [
-            0 => ['nome' => 'fornecedor 1', 'status' => 'N', 'cnpj' => '00.000.000/000-00'],
+            0 => ['nome' => 'fornecedor 1', 'status' => 'N', 'cnpj' => ''],
             1 => ['nome' => 'fornecedor 2', 'status' => 'S']
         ];
 
