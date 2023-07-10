83d38216212761b23488bfa5af3a47739fcc28cb
Matheus França <matheussss6613@gmail.com>
2023-07-10 17:14:33 -0300

    test empty

--- resources/views/app/suppliers/index.blade.php
+++ resources/views/app/suppliers/index.blade.php
@@ -8,17 +8,21 @@
 
 @php
     // if(isset($variavel)) {} //retorna se a variável está definida
+    // if(empty($variavel)) {} //retorna true se a variável vazia
 @endphp
 
 
 <!-- Testa a existência da variável, caso exista, ela entra no método, caso não, apenas deixa de lado -->
 @isset($suppliers)
-    Fornecedor: {{ $suppliers[1]['nome'] }}
+    Fornecedor: {{ $suppliers[0]['nome'] }}
     <br>
-    Status: {{ $suppliers[1]['status'] }}
+    Status: {{ $suppliers[0]['status'] }}
     <br>
-    @isset($suppliers[1]['cnpj']) <!-- caso exista ele funciona, caso não, não funciona -->
+    @isset($suppliers[0]['cnpj']) <!-- caso exista ele funciona, caso não, não funciona -->
         CNPJ: {{ $suppliers[0]['cnpj'] }}
+        @empty($suppliers[0]['cnpj']) <!-- teste para ver se está vazio -->
+            - vazio
+        @endempty
     @endisset
 @endisset
 
