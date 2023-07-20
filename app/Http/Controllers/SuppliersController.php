5b3ec4448d0814b76036695b6beb3b570ecf4200
Matheus França <matheussss6613@gmail.com>
2023-07-20 13:58:00 -0300

    add ddd & telefone

--- app/Http/Controllers/SuppliersController.php
+++ app/Http/Controllers/SuppliersController.php
@@ -10,8 +10,28 @@ class SuppliersController extends Controller
     public function index()
     {
         $suppliers = [
-            0 => ['nome' => 'fornecedor 1', 'status' => 'N', 'cnpj' => ''],
-            1 => ['nome' => 'fornecedor 2', 'status' => 'S']
+            0 => [
+                'nome' => 'fornecedor 1',
+                'status' => 'N',
+                'cnpj' => '00.000.000/0000-00',
+                'ddd' => '11', //São Paulo
+                'telefone' => '0000-0000'
+
+            ],
+            1 => [
+                'nome' => 'fornecedor 2',
+                'status' => 'S',
+                'cnpj' => null,
+                'ddd' => '85', //Fortaleza
+                'telefone' => '0000-0000'
+            ],
+            2 => [
+                'nome' => 'fornecedor 3',
+                'status' => 'S',
+                'cnpj' => null,
+                'ddd' => '32', //Juiz de Fora
+                'telefone' => '0000-0000'
+            ]
         ];
 
         return view('app.suppliers.index', compact('suppliers'));
